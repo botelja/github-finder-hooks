d1e3c71a77b7f7d741e54e9161db3ebae8ef5ba7
botelja <ivo.surjak@gmail.com>
2019-10-30 14:13:37 +0100

    Refactor user to functional component and add useEffect

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,100 +1,98 @@
-import React, { Component, Fragment } from 'react';
+import React, { Fragment, useEffect } from 'react';
 import Spinner from './Spinner';
 import Repos from './Repos';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
-export class User extends Component {
-  componentDidMount() {
-    this.props.getUser(this.props.match.params.login);
-    this.props.getUserRepos(this.props.match.params.login);
-  }
-  render() {
-    const {
-      name,
-      company,
-      avatar_url,
-      location,
-      blog,
-      login,
-      html_url,
-      followers,
-      following,
-      public_repos,
-      public_gists,
-      hireable
-    } = this.props.user;
+const User = ({ user, loading, repos, getUser, getUserRepos, match }) => {
+  useEffect(() => {
+    getUser(match.params.login);
+    getUserRepos(match.params.login);
+    //eslint-disable-next-line
+  }, []);
 
-    const { loading, repos } = this.props;
+  const {
+    name,
+    company,
+    avatar_url,
+    location,
+    blog,
+    login,
+    html_url,
+    followers,
+    following,
+    public_repos,
+    public_gists,
+    hireable
+  } = user;
 
-    if (loading) return <Spinner />;
+  if (loading) return <Spinner />;
 
-    return (
-      <Fragment>
-        <Link to="/" className="btn btn-light mr-3">
-          Back to Search
-        </Link>
-        Hireable:{' '}
-        {hireable ? (
-          <i className="fas fa-check"></i>
-        ) : (
-          <i class="fas fa-times-circle"></i>
-        )}
-        <div className="card my-3">
-          <img
-            src={avatar_url}
-            className="my-3  mx-auto d-block rounded-circle"
-            alt="..."
-            style={{ width: '160px' }}
-          />
-          <div className="card-body">
-            <h5 className="card-title">{name}</h5>
-            <p className="card-text">Location: {location}</p>
-            <a href={html_url} className="btn btn-primary">
-              Visit Github Profile
-            </a>
-          </div>
-          <ul className="list-group">
-            <li className="list-group-item">
-              {login && (
-                <Fragment>
-                  <strong>Username:</strong> {login}
-                </Fragment>
-              )}
-            </li>
-            <li className="list-group-item">
-              {company && (
-                <Fragment>
-                  <strong>Company:</strong> {company}
-                </Fragment>
-              )}
-            </li>
-            <li className="list-group-item">
-              {blog && (
-                <Fragment>
-                  <strong>Website:</strong> {blog}
-                </Fragment>
-              )}
-            </li>
-          </ul>
+  return (
+    <Fragment>
+      <Link to="/" className="btn btn-light mr-3">
+        Back to Search
+      </Link>
+      Hireable:{' '}
+      {hireable ? (
+        <i className="fas fa-check"></i>
+      ) : (
+        <i className="fas fa-times-circle"></i>
+      )}
+      <div className="card my-3">
+        <img
+          src={avatar_url}
+          className="my-3  mx-auto d-block rounded-circle"
+          alt="..."
+          style={{ width: '160px' }}
+        />
+        <div className="card-body">
+          <h5 className="card-title">{name}</h5>
+          <p className="card-text">Location: {location}</p>
+          <a href={html_url} className="btn btn-primary">
+            Visit Github Profile
+          </a>
         </div>
-        <span className="badge badge-pill badge-primary">
-          Followers: {followers}
-        </span>
-        <span className="badge badge-pill badge-success ml-1">
-          Following: {following}
-        </span>
-        <span className="badge badge-pill badge-warning ml-1">
-          Public Repos: {public_repos}
-        </span>
-        <span className="badge badge-pill badge-secondary ml-1">
-          Public Gists: {public_gists}
-        </span>
-        <Repos repos={repos} />
-      </Fragment>
-    );
-  }
-}
+        <ul className="list-group">
+          <li className="list-group-item">
+            {login && (
+              <Fragment>
+                <strong>Username:</strong> {login}
+              </Fragment>
+            )}
+          </li>
+          <li className="list-group-item">
+            {company && (
+              <Fragment>
+                <strong>Company:</strong> {company}
+              </Fragment>
+            )}
+          </li>
+          <li className="list-group-item">
+            {blog && (
+              <Fragment>
+                <strong>Website:</strong> {blog}
+              </Fragment>
+            )}
+          </li>
+        </ul>
+      </div>
+      <span className="badge badge-pill badge-primary">
+        Followers: {followers}
+      </span>
+      <span className="badge badge-pill badge-success ml-1">
+        Following: {following}
+      </span>
+      <span className="badge badge-pill badge-warning ml-1">
+        Public Repos: {public_repos}
+      </span>
+      <span className="badge badge-pill badge-secondary ml-1">
+        Public Gists: {public_gists}
+      </span>
+      <Repos repos={repos} />
+    </Fragment>
+  );
+};
 
 User.propTypes = {
   loading: PropTypes.bool.isRequired,

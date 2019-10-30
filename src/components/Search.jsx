f84fe3d63c41f06d9aeda13d67605ea4f4c38766
botelja <ivo.surjak@gmail.com>
2019-10-30 13:57:20 +0100

    Refactor Search to functional and hooks

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,59 +1,50 @@
-import React, { Component, Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 import PropTypes from 'prop-types';
 
-class Search extends Component {
-  constructor() {
-    super();
-    this.state = {
-      name: ''
-    };
-  }
+const Search = ({ searchUsers, clearUsers, showClear, showAlert }) => {
+  const [name, setName] = useState('');
 
-  handleSubmit = (e) => {
+  const handleSubmit = (e) => {
     e.preventDefault();
-    if (this.state.name === '') {
-      this.props.showAlert('Please enter some name', 'danger');
+    if (name === '') {
+      showAlert('Please enter some name', 'danger');
     } else {
-      this.props.searchUsers(this.state.name);
-      this.setState({ name: '' });
+      searchUsers(name);
+      setName('');
     }
   };
 
-  handleChange = (e) => {
-    this.setState({ [e.target.name]: e.target.value });
+  const handleChange = (e) => {
+    setName(e.target.value);
   };
 
-  render() {
-    const { name } = this.state;
-    const { showClear, clearUsers } = this.props;
-    return (
-      <Fragment>
-        <form onSubmit={this.handleSubmit}>
-          <div className="form-group">
-            <input
-              type="text"
-              className="form-control"
-              id="name"
-              name="name"
-              value={name}
-              onChange={this.handleChange}
-              placeholder="Search name..."
-            />
-          </div>
+  return (
+    <Fragment>
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <input
+            type="text"
+            className="form-control"
+            id="name"
+            name="name"
+            value={name}
+            onChange={handleChange}
+            placeholder="Search name..."
+          />
+        </div>
 
-          <button type="submit" className="btn btn-dark btn-block">
-            Submit
-          </button>
-        </form>
-        {showClear && (
-          <button className="btn btn-light btn-block mt-2" onClick={clearUsers}>
-            Clear
-          </button>
-        )}
-      </Fragment>
-    );
-  }
-}
+        <button type="submit" className="btn btn-dark btn-block">
+          Submit
+        </button>
+      </form>
+      {showClear && (
+        <button className="btn btn-light btn-block my-2" onClick={clearUsers}>
+          Clear
+        </button>
+      )}
+    </Fragment>
+  );
+};
 
 Search.propTypes = {
   searchUsers: PropTypes.func.isRequired,
